b30197f1b9932c8eecf61ed2d0fc3fcd4f7b55c7
Mohammadreza Lotfi <138774907+mohammadrezalotfii@users.noreply.github.com>
2026-06-26 18:23:58 +0330

    V1.0.1
    fixed and refactor JS code

--- script.js
+++ script.js
@@ -6,15 +6,20 @@ const securNumber = Math.trunc(Math.random() * 100) + 1;
 let scoreElement = document.querySelector(".score");
 let score = Number(scoreElement.textContent);
 let highscore = 0;
+
 const checkBTN = document.querySelector(".check");
 const againBTN = document.querySelector(".again");
+
+const selectorText = (selectorClass, text) => {
+  document.querySelector(`.${selectorClass}`).textContent = text;
+};
 
 checkBTN.addEventListener("click", () => {
   let guess = Number(document.querySelector(".guess").value);
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔ No Number!";
+    selectorText("message", "⛔ No Number!");
   } else if (guess == securNumber) {
-    document.querySelector(".message").textContent = "🎉 Correct Number!";
+    selectorText("message", "🎉 Correct Number!");
 
     document.querySelector(".number").textContent = securNumber;
 
@@ -22,14 +27,11 @@ checkBTN.addEventListener("click", () => {
 
     highscore += score;
     document.querySelector(".highscore").textContent = highscore;
-  } else if (guess > securNumber) {
-    document.querySelector(".message").textContent = "📈 Too high!";
-    score--;
-    score >= 0
-      ? (scoreElement.textContent = score)
-      : (scoreElement.textContent = 0);
-  } else if (guess < securNumber) {
-    document.querySelector(".message").textContent = "📉 Too low!";
+  } else if (guess !== securNumber) {
+    selectorText(
+      "message",
+      guess > securNumber ? "📈 Too high!" : "📉 Too low!",
+    );
     score--;
     score >= 0
       ? (scoreElement.textContent = score)
@@ -40,6 +42,7 @@ checkBTN.addEventListener("click", () => {
 againBTN.addEventListener("click", () => {
   const securNumber = Math.trunc(Math.random() * 100) + 1;
   document.querySelector("body").style.backgroundColor = "#222";
+  selectorText("message", "Start guessing...");
   score = 20;
   scoreElement.textContent = score;
   document.querySelector(".guess").value = "";
